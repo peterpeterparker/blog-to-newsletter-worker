7b37a51d9ef281f6cb3e01065a667a9857f7ca66
David Dal Busco <david.dalbusco@outlook.com>
2026-06-04 17:37:46 +0200

    feat: replace Cloudflare Worker with Kyushu (#2)
    * feat: replace Cloudflare Worker with Kyushu
    
    * feat: enable legacy decorator for kyu build
    
    * test: vitest only

--- src/callback/_callback.ts
+++ src/callback/_callback.ts
@@ -1,7 +1,8 @@
 import type { Factory } from "../common/types.ts";
+import type { WorkerRequest, WorkerResponse } from "kyushu-types";
 
 interface Callback {
-  handle(params: { url: URL; request: Request }): Promise<Response>;
+  handle(params: { url: URL; request: WorkerRequest }): Promise<WorkerResponse>;
 }
 
 export function CallbackProvider(_constructor: Factory<Callback>) {}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-import { Env, EnvSchema } from "./common/types";
+import { type Env, EnvSchema } from "./common/types";
 import { TelegramCallback } from "./callback/telegram/telegram";
+import type { ExportedHandler } from "kyushu-types";
 
 export default {
-  async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+  async fetch(request) {
     if (request.method !== "POST") {
-      return new Response("Method Not Allowed", { status: 405 });
+      return {
+        status: 405,
+        body: "Method Not Allowed",
+      };
     }
 
-    const envParsed = EnvSchema.safeParse(env);
+    const processEnv: Partial<Env> = {
+      TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+      TELEGRAM_SECRET: process.env.TELEGRAM_SECRET,
+      MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+    };
+
+    const envParsed = EnvSchema.safeParse(processEnv);
     if (!envParsed.success) {
-      return new Response("Misconfigured", { status: 500 });
+      return {
+        status: 500,
+        body: "Misconfigured",
+      };
     }
+
+    const { data: env } = envParsed;
 
     const url = URL.parse(request.url);
 
     if (url === null) {
-      return new Response("Forbidden", { status: 403 });
+      return {
+        status: 403,
+        body: "Forbidden",
+      };
     }
 
     const { pathname } = url;
@@ -24,6 +42,9 @@ export default {
       return await TelegramCallback.create(env).handle({ url, request });
     }
 
-    return new Response("Bad Request", { status: 400 });
+    return {
+      status: 400,
+      body: "Bad Request",
+    };
   },
-};
+} satisfies ExportedHandler;
